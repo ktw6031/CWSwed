f07a8c9ba443f1dc9ee2711993e5a078d13d2b13
redStoneKim <hongseok0610@gmail.com>
2024-10-30 00:06:18 +0900

    기초 설계!

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,6 +7,9 @@ export default defineConfig({
         watch: {
             usePolling: true,    // 파일 변경 감지를 위한 폴링 활성화
         },
+    },
+    build: {
+        outDir: 'docs'  // 빌드 출력 디렉토리를 'docs'로 설정
     },
     css: {
         preprocessorOptions: {
