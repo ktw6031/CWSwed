bd5d3b2b5721f13760c0483985912aae55507aae
redStoneKim <hongseok0610@gmail.com>
2024-10-29 23:19:48 +0900

    기초 설계

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import '/src/scss/index.css'
+
 function App() {
     return (
-        <div>
-      <h1>Hello, React 18 + Vite!</h1>
-    </div>
+        <div className='main'>
+           <div className='sect sect_1'>
+               <div className='video_con'>
+                   <video autoPlay muted loop>
+                       <source src="/video/2802271-hd_1920_1080_30fps.mp4" type="video/mp4" />
+                   </video>
+               </div>
+               <div className='text_con'>
+                   <div className='logo'>
+                       로고
+                   </div>
+                   <div className='dsc'>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi consequatur dicta eligendi, illum modi obcaecati pariatur, quam quis, rem totam voluptate. Beatae eveniet explicabo quod tempore temporibus veritatis vero!</p>
+                   </div>
+               </div>
+           </div>
+           <div className='sect sect_2'>
+               <div className='top_bar'></div>
+               <div className='box_con_wrap' >
+                    <div className='box_con' >
+                        <div className='box' ></div >
+                        <div className='box' ></div >
+                        <div className='box' ></div >
+                        <div className='box' ></div >
+                        <div className='box' ></div >
+                        <div className='box' ></div >
+                    </div >
+               </div >
+           </div >
+       </div >
     )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,    // 파일 변경 감지를 위한 폴링 활성화
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "./src/scss/index.scss;`
+            }
+        }
+    }
 })
